ff4fcc35c4a276120a864d599a578382bb9cfcaa
bpearls214 <48865572+bpearls214@users.noreply.github.com>
2019-06-13 16:29:22 -0400

    added repository, loop and conditional

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,21 @@
-alert('Hello world');
+var repository = [
+    {name: 'Vulpix',
+    height: 3,
+    type: ['flamethrower',' speed']},
+
+    {name: 'Pikachu',
+    height: 2,
+    type: ['lightning',' humor']},
+
+    {name: 'Squidward',
+    height: 4,
+    type: ['water',' strength']},
+  ];
+
+for (var i = 0; i < repository.length; i++) {
+  document.write('<br/>', repository[i].name);
+  document.write(" (" + repository[i].height + ' feet, Type: ' + repository[i].type + ')');
+  if (repository[i].height === 4) {
+    document.write('....yikes, that\'s big!')
+  }
+}
