d9e4412a23174b79663659b08a7f9168f7866b68
bpearls214 <48865572+bpearls214@users.noreply.github.com>
2019-06-14 15:21:28 -0400

    added forEach function and used input for multiple arrays

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 var repository = [
+  // pokemon objects
     {name: 'Vulpix',
     height: 3,
     type: ['flamethrower',' speed']},
@@ -12,10 +13,37 @@ var repository = [
     type: ['water',' strength']},
   ];
 
-for (var i = 0; i < repository.length; i++) {
-  document.write('<br/>', repository[i].name);
-  document.write(" (" + repository[i].height + ' feet, Type: ' + repository[i].type + ')');
-  if (repository[i].height === 4) {
-    document.write('....yikes, that\'s big!')
+  var repository2 = [
+    // pokemon objects
+      {name: 'Bigfoot',
+      height: 11,
+      type: ['smell',' strength']},
+
+      {name: 'Loch Nessie',
+      height: 4,
+      type: ['water',' stealth']},
+    ];
+
+  /* ---old 'for' function, excluded to include for each instead 6/14/19 --
+ function printArrayDetails(pokemonList) {
+  for (var i = 0; i < pokemonList.length; i++) {
+    document.write('<br/>', pokemonList[i].name);
+    document.write(" (" + pokemonList[i].height + ' feet, Type: ' + pokemonList[i].type + ')');
+    if (pokemonList[i].height === 4) {
+      document.write('....yikes, that\'s big!')
+    }
   }
 }
+
+// Calling printArrayDetails function twice
+printArrayDetails(repository);
+printArrayDetails(repository2);
+*/
+function printArrayDetails(pokemonList) {
+  pokemonList.forEach(function(property){
+    document.write(property.name + ': ' + property.height + ' feet </br>')
+  })
+}
+
+printArrayDetails(repository);
+printArrayDetails(repository2);
